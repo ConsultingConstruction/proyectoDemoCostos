3f525569e7f4bc42542c10cbde6403e8c1e960e0
Jose <jose.cano@consulting.construction.corp>
2022-05-20 11:27:46 -0500

    Se añadio descripcion y consecutivo

--- ProyectoDemoCostosApp/views.py
+++ ProyectoDemoCostosApp/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from ProyectoDemoCostosApp.models import Acronimo, AguaCemento, OMC23Nivel1, OMC23Nivel2, OMC23Nivel3, OMC23Nivel4, OMC23Nivel5,OMC23Nivel6, OmniClass23, OmniClass41, TipoConsistencia, TipoUniMed
 from ProyectoDemoCostosApp.models import Materiales, Concreto, Esfuerzo, ValorEsfuerzo, TipoResistencia, AplPrincipales, TMA, Revenimiento, Densidad, SistColocacion, ClasExposicion, FlujoRev, CaracEspe, IonCloruro, FibraConcre, UnidadesMedida
@@ -51,52 +52,87 @@ def obtenerMaterial(request, idOmc, numMat): #esta funcion por parametro va a re
 def obtenerConcreto(request, idOmc, pNumMat): #esta funcion por parametro va a re recibir el identificador del registro que se selecciono  y el numero identificador del material (numMat)
     
     registroOMC = get_object_or_404(OmniClass23, pk=idOmc)
+    #--hacer lo correspondiente para generar el consecutivo
+    ultimoRegistro = Materiales.objects.all().last()
+    ultimoRegistro = generarConsecutivo(ultimoRegistro.Consecutivo)
+    #print('consecutivo:' + ultimoRegistro)
     if request.method == 'POST':
         formCaracEspe = CaracEspeForm(request.POST)
         if formCaracEspe.is_valid():
-            #--HACER FUNCION O TRIGGER O PROCEDURE QUE VALIDE QUE EL CONCRETO NO EXISTE YA
+            #--VALIDAR SI EXISTE EL CONCRETO
+            #>>HACER FUNCION O TRIGGER O PROCEDURE QUE VALIDE QUE EL CONCRETO NO EXISTE YA
+            #--SE GENERA LA DESCRIPCION CORTA Y LARGA
+            registroEsfuerzo = ''
+            registroUMD = '' 
+            registroUMFR = ''
+            registroAplPrin = None
+            cadRegistroAplPrin = ''
+            registroDensidad = None
+            cadRegistroDensidad = ''
+            registroClasExpo = None
+            cadRegistroClasExpo = ''
+            registroFlujoRev = None
+            cadRegistroFlujoRev = ''
+            registroFibraCon = None
+            cadRegistroFibraCon = ''
+            registroIonClor = None
+            cadRegistroIonClor = '' 
+            if request.POST.get('idEsfuerzo'):
+                registroEsfuerzo = get_object_or_404(Esfuerzo,pk = request.POST.get('idEsfuerzo'))
+                registroEsfuerzo = registroEsfuerzo.tipoEsfuerzo
+            registroValEsf = get_object_or_404(ValorEsfuerzo, pk = request.POST.get('idValEsf'))
+            registroUMVE = get_object_or_404(UnidadesMedida, pk = request.POST.get('idUniMedVE'))
+            registroTipoResist = get_object_or_404(TipoResistencia, pk = request.POST.get('idTipoResist'))
+            if request.POST.get('idAplPrinc'):
+                registroAplPrin = get_object_or_404(AplPrincipales, pk = request.POST.get('idAplPrinc'))
+                cadRegistroAplPrin = registroAplPrin.aplicaciones
+            registroTma = get_object_or_404(TMA, pk = request.POST.get('idTma'))
+            registroUMTMA = get_object_or_404(UnidadesMedida, pk = request.POST.get('idUniMedTMA'))
+            registroReven = get_object_or_404(Revenimiento, pk = request.POST.get('idReven'))
+            registroUMR = get_object_or_404(UnidadesMedida, pk = request.POST.get('idUniMedR'))
+            if request.POST.get('idDensidad'):
+                registroDensidad = get_object_or_404(Densidad,pk = request.POST.get('idDensidad'))
+                cadRegistroDensidad = 'Densidad=' + str(registroDensidad.valDensidad).strip()
+            if request.POST.get('idUniMedD'):
+                registroUMD = get_object_or_404(UnidadesMedida, pk = request.POST.get('idUniMedD'))
+                registroUMD = registroUMD.Unidad
+            registroSistColoc = get_object_or_404(SistColocacion,pk = request.POST.get('idSistColoc'))
+            if request.POST.get('idClasExpo'):
+                registroClasExpo = get_object_or_404(ClasExposicion,pk = request.POST.get('idClasExpo'))
+                cadRegistroClasExpo = 'ClaseExp=' + str(registroClasExpo.Clase).strip()
+            if request.POST.get('idFlujoRev'):
+                registroFlujoRev = get_object_or_404(FlujoRev, pk = request.POST.get('idFlujoRev'))
+                cadRegistroFlujoRev = 'FlujoRev=' + str(registroFlujoRev.valFluRev).strip()
+            if request.POST.get('idUniMedFR'):
+                registroUMFR = get_object_or_404(UnidadesMedida, pk = request.POST.get('idUniMedFR'))
+                registroUMFR = registroUMFR.Unidad
+            if request.POST.get('idFibraConcre'):
+                registroFibraCon = get_object_or_404(FibraConcre, pk = request.POST.get('idFibraConcre'))
+                cadRegistroFibraCon = registroFibraCon.Fibras
+            if request.POST.get('idIonClor'):
+                registroIonClor = get_object_or_404(IonCloruro, pk = request.POST.get('idIonClor'))
+                cadRegistroIonClor = registroIonClor.cargaPesada
+            newForm = formCaracEspe.save(commit=False)
+            conector = ' '
+            
+            auxDescriCorta = str(registroOMC.descriSpa).strip() + conector + str(registroEsfuerzo).strip() + conector + 'fc=' + str(registroValEsf.Valor).strip() + str(registroUMVE.Unidad).strip() + conector + str(registroTipoResist.Tipo).strip() + conector + str(cadRegistroAplPrin).strip() + conector + 'Tma=' + str(registroTma.valTma).strip() + str(registroUMTMA.Unidad).strip() + conector + 'Rev=' + str(registroReven.valRev).strip() + str(registroUMR.Unidad).strip() + conector + cadRegistroDensidad + str(registroUMD).strip() + conector + str(registroSistColoc.tipoSistema).strip() + conector + cadRegistroClasExpo + conector + cadRegistroFlujoRev + str(registroUMFR).strip()
+            cadDescriCorta = auxDescriCorta[0:100]
+            cadDescriLarga = auxDescriCorta + conector + ValidaCadena('Abrev=',newForm.Acronimo,'') + conector + ValidaCadena('Modulo de Elasticidad=',newForm.modElast,'') + conector + ValidaCadena('Edad=',newForm.Edad,'dias') + conector + ValidaCadena('Absorción Capilar=',newForm.absorcionCap,'') + conector + ValidaCadena('Abrev2=',newForm.Acronimo2,'') + conector + ValidaCadena('Trabajabilidad Extendidaa=',newForm.trabaExtend,'hrs') + conector + ValidaCadena('Clase=',newForm.Clase,'') + conector + ValidaCadena('Color=',newForm.Color,'') + conector + ValidaCadena('Comportamiento=',newForm.Comportamiento,'') + conector + ValidaCadena('Contenido de aire=',newForm.conAire,'%') + conector + ValidaCadena('Contenido de Ion Cloruro=',newForm.conIonClor,'%') + conector + ValidaCadena('Ion Cloruro=',cadRegistroIonClor, 'coulomb') + conector + ValidaCadena('Tiempo de prueba del ensayo Ion Cloruro=',newForm.tiempoPrueba,'dias') + conector + ValidaCadena('Fibra=',cadRegistroFibraCon,'') + conector + ValidaCadena('Comentarios=',request.POST.get('comentarios'),'') + conector + ValidaCadena('Palabras Clave=',request.POST.get('palabrasCve'),'')
             #--INSERTAR EN LA TABLA DE MATERIALES
-                #--hacer lo correspondiente para generar el consecutivo
-                #--hacer lo correspondiente para la descripcion corta
-            #descriCorta = 
-                #--hacer lo correspondiente para la descripcion larga
             #cadena1 ='numMat:' + str(pNumMat) + ',codigoOmc:' + registroOMC.Codigo + ',Consecutivo:' + ',descriCorta:' + ',descriLarga:' + ',Comentarios:' + str(request.POST.get('comentarios')) + ',PalabrasCve:' + str(request.POST.get('palabrasCve')) + ',codigoBimsa:' + ',fk_Omc23:' + str(idOmc) + ',fk_omc41:'
-            #print(cadena1)
-            materiales = Materiales(numMat = pNumMat, codigoOmc = registroOMC.Codigo, Consecutivo = None, descriCorta = None, descriLarga = None, Comentarios = request.POST.get('comentarios'), palabrasCve = request.POST.get('palabrasCve'), codigoBimsa = None, fk_Omc23 = registroOMC, fk_Omc41 = None)
+            #print(descriLarga)
+            materiales = Materiales(numMat = pNumMat, codigoOmc = registroOMC.Codigo, Consecutivo = ultimoRegistro, descriCorta = cadDescriCorta, descriLarga = cadDescriLarga, Comentarios = request.POST.get('comentarios'), palabrasCve = request.POST.get('palabrasCve'), codigoBimsa = None, fk_Omc23 = registroOMC, fk_Omc41 = None)
             materiales.save()
             #--INSERTAR EN LA TABLA DE CONCRETO
             #cadena2 = 'numMat:' + str(pNumMat) + ',Codigo:' + str(request.POST.get('codigoOmc')) + ',fk_Material:' + ',fk_ClasExpo:' + str(request.POST.get('idClasExpo')) + ',fk_SistColoc:' + str(request.POST.get('idSistColoc')) + ',fk_Densidad:' + str(request.POST.get('idDensidad')) + ',fk_Reven:' + str(request.POST.get('idReven')) + ',fk_FlujoRev:' + str(request.POST.get('idFlujoRev')) + ',fk_FibraConcre:' + str(request.POST.get('idFibraConcre')) + ',fk_ValEsf:' + str(request.POST.get('idValEsf')) + ',fk_Tma:' + str(request.POST.get('idTma')) + ',fk_AplPrinc:' + str(request.POST.get('idAplPrinc'))
             #print(cadena2)
-            registroClasExpo = None
-            registroDensidad = None
-            registroFlujoRev = None
-            registroFibraCon = None
-            registroAplPrin = None
-            if request.POST.get('idClasExpo'):
-                registroClasExpo = get_object_or_404(ClasExposicion,pk = request.POST.get('idClasExpo'))
-            registroSistColoc = get_object_or_404(SistColocacion,pk = request.POST.get('idSistColoc'))
-            if request.POST.get('idDensidad'):
-                registroDensidad = get_object_or_404(Densidad,pk = request.POST.get('idDensidad'))
-            registroReven = get_object_or_404(Revenimiento, pk = request.POST.get('idReven'))
-            if request.POST.get('idFlujoRev'):
-                registroFlujoRev = get_object_or_404(FlujoRev, pk = request.POST.get('idFlujoRev'))
-            if request.POST.get('idFibraConcre'):
-                registroFibraCon = get_object_or_404(FibraConcre, pk = request.POST.get('idFibraConcre'))
-            registroValEsf = get_object_or_404(ValorEsfuerzo, pk = request.POST.get('idValEsf'))
-            registroTma = get_object_or_404(TMA, pk = request.POST.get('idTma'))
-            if request.POST.get('idAplPrinc'):
-                registroAplPrin = get_object_or_404(AplPrincipales, pk = request.POST.get('idAplPrinc'))
-
             concreto = Concreto(numMat = pNumMat, Codigo = registroOMC.Codigo, fk_Material = materiales, fk_ClasExpo = registroClasExpo, fk_SistColoc = registroSistColoc, fk_Densidad = registroDensidad, fk_Reven = registroReven, fk_FlujoRev = registroFlujoRev, fk_FibraConcre = registroFibraCon, fk_ValEsf = registroValEsf, fk_Tma = registroTma, fk_AplPrinc = registroAplPrin)
             concreto.save()
             #--INSERTAR EN LA TABLA DE CARACESPE
             #newForm = formCaracEspe.save(commit=False)
             #cadena3 = str(newForm.Acronimo) + '-' + str(newForm.modElast) + '-' + str(newForm.Edad) + '-' + str(newForm.absorcionCap) + '-' + str(newForm.Acronimo2) + '-' + str(newForm.trabaExtend) + '-' + str(newForm.Clase) + '-' + str(newForm.Color) + '-' + str(newForm.Comportamiento) + '-' + str(newForm.conAire) + '-' + str(newForm.conIonClor) + '-' + str(request.POST.get('idIonClor') + '-' + str(newForm.tiempoPrueba))
             #print(cadena3)
-            newForm = formCaracEspe.save(commit=False)
-            if request.POST.get('idIonClor'):
-                registroIonClor = get_object_or_404(IonCloruro, pk = request.POST.get('idIonClor'))
-                newForm.fk_IonClor = registroIonClor
+            newForm.fk_IonClor = registroIonClor
             newForm.fk_Concreto = concreto
             newForm.save()
             return redirect('Principal')
@@ -130,5 +166,16 @@ def obtenerConcreto(request, idOmc, pNumMat): #esta funcion por parametro va a r
         #cadena = 'aqui concatenacion ' + str(123)
         #print(cadena)
 
-    return render(request, "ProyectoDemoCostosApp/concreto.html", {'listaOMC': listaOMC, 'registroOMC': registroOMC, 'formCaracEspe':formCaracEspe,'datEsfuerzo': datEsfuerzo,'datValorEsfuerzo': datValorEsfuerzo, 'datTipoResistencia':datTipoResistencia, 'datAplPrincipales':datAplPrincipales, 'daTma':daTma, 'datRevenimiento':datRevenimiento, 'datDensidad':datDensidad, 'datSisColocacion':datSisColocacion,'datClasExposicion':datClasExposicion,'datFlujoRev':datFlujoRev,'datIonCloruro':datIonCloruro,'datFibraConcre':datFibraConcre,'datUnidadesMedida':datUnidadesMedida})
+    return render(request, "ProyectoDemoCostosApp/concreto.html", {'listaOMC': listaOMC, 'registroOMC': registroOMC, 'ultimoRegistro':ultimoRegistro,'formCaracEspe':formCaracEspe,'datEsfuerzo': datEsfuerzo,'datValorEsfuerzo': datValorEsfuerzo, 'datTipoResistencia':datTipoResistencia, 'datAplPrincipales':datAplPrincipales, 'daTma':daTma, 'datRevenimiento':datRevenimiento, 'datDensidad':datDensidad, 'datSisColocacion':datSisColocacion,'datClasExposicion':datClasExposicion,'datFlujoRev':datFlujoRev,'datIonCloruro':datIonCloruro,'datFibraConcre':datFibraConcre,'datUnidadesMedida':datUnidadesMedida})
 
+def ValidaCadena(label, dato, adicion):
+    if not dato:
+        return ''
+    else:
+        return label + str(dato).strip() + adicion
+
+def generarConsecutivo(ultimoConsecutivo):
+    consecutivoInt = int(ultimoConsecutivo)
+    consecutivoInt = consecutivoInt + 1
+    consecutivoInt = str(consecutivoInt).rjust(5,'0')
+    return str(consecutivoInt)
